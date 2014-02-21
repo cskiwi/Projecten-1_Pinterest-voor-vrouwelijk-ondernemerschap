23c1e0805a8bcebc0efef15b7fb6453b43ee8e12
Glenn Latomme <glenn.latomme@gmail.com>
2014-02-21 18:00:22 +0100

    How to setup databse

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 header('location:./public');
 exit();
+
+/**
+ * ====================
+ * Setup database
+ * ====================
+ * create the database defined in app/config/database.php
+ *
+ * type:
+ * php artisan migrate
+ * php db:seed
+ *
+ *
+ */
