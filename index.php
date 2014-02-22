551de8c56a4ead664cf8d3d2cca6ec6ce438933e
Glenn Latomme <glenn.latomme@gmail.com>
2014-02-22 21:12:47 +0100

    updated info on the artisan command

--- index.php
+++ index.php
@@ -15,7 +15,11 @@ exit();
  * ====================
  * create the database defined in app/config/database.php
  *
- * type in cmd in root folder:
- * 'php artisan migrate:refresh --seed'
+ * type in cmd in root folder
+ * 'php artisan migrate:refresh --seed' 
+ * note: 
+ *    - seed is optional for filling with dummy data
+ *    - refresh is if you have an older version installed of this app so everything get's flushed
+ * 
  *
  */
