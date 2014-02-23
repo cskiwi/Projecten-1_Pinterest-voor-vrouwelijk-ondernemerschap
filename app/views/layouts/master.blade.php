46c338049028f4aa85e4a59fa5594947aa136731
Jessestr <jesse.struyvelt@telenet.be>
2014-02-23 14:48:59 +0100

    Bootstrap included in main template

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,13 +1,27 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en" class="no-js">
 <head>
     <meta charset="utf-8">
     {{ HTML::style('css/common.css'); }}
     {{ HTML::script('js/testscript.js'); }}
     <title>@yield('pagetitle')</title>
+
+    <meta name="description" content="">
+    <meta name="robots" content="">
+	<meta name="viewport" content="width=device-width,initial-scale=1">	
+	
+
+    <link rel="shortcut icon" href="" />
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700|Dosis:200,600|Open+Sans:400,700" type="text/css" />
+	{{ HTML::style('css/bootstrap.min.css') }}
+	{{ HTML::style('css/bootstrap-theme.min.css') }}
+	{{ HTML::style('css/font-awesome.min.css') }}
+	{{ HTML::style('css/default.css') }}
+	
 </head>
 
 <body>
+
 <div id="nav">
     <ul>
         <li><a href="{{ URL::TO('/') }}">home</a></li>
@@ -21,6 +35,11 @@
         @endif
     </ul>
 </div>
+
 @yield('content')
+
+	<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	{{ HTML::script('js/bootstrap.min.js') }}
 </body>
 </html>
