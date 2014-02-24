f30903b374009bf157aac7571a9e24ad503ba580
Jessestr <jesse.struyvelt@telenet.be>
2014-02-24 15:53:01 +0100

    Homepage design alpha
    Started designing

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,21 +1,139 @@
 @extends('layouts.master')
 
+<!--<div id="nav">
+    <ul>
+        <li><a href="{{ URL::TO('/') }}">home</a></li>
+        @if(Auth::check())
+        <li><a href="{{ URL::TO('admin/logout') }}">logout</a></li>
+        <li><a href="{{ URL::TO('admin/profile') }}">{{ Auth::user()->name  }} ({{ Auth::user()->username  }})</a></li>
+        @else
+        @if(!(Request::is('admin/login') || Request::is('admin/register')))
+            <li><a href="{{ URL::TO('admin/login') }}">Login</a> or <a href="{{ URL::TO('admin/register') }}">register</a></li>
+        @endif
+        @endif
+    </ul>
+</div>
+
+-->
+
 @section('pagetitle')
-Welcome page
+	Project PVVO
 @stop
 
+
 @section('content')
+<div class="jumbotron pvvoJumbo">
+
+	<div class="container">
+	
+	<nav class="navbar navbar-default pvvoNavbar" role="navigation">
+	  <div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand pvvoTitle" href="#">WomanInterest</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav">
+	
+		  </ul>
+		
+		  <ul class="nav navbar-nav navbar-right">
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="fa fa-bars rightSpacingSmall"> </span> USERS </b></a>
+				<ul class="dropdown-menu pvvoDropdown text-right">
+					<li class="divider"></li>
+					<li><a href="{{ URL::to('users') }}">Login</a></li>
+					<li><a href="{{ URL::to('users') }}">Register</a></li>
+					<li><a href="{{ URL::to('users') }}">Privacy</a></li>
+				</ul>
+			</li>
+			<li><a href="{{ URL::to('posts') }}"><span class="fa fa-comments rightSpacingSmall"> </span> POSTS</a></li>
+			<li><a href="{{ URL::to('boards') }}"><span class="fa fa-cloud rightSpacingSmall"> </span> BOARDS</a></li>
+		  </ul>
+		</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+	
+		<div class="row">
+		
+			<div class="col-md-7 topOffset text-center">
+		
+				<h2>Hello, ladies.. <br />Get ready for some interesting times.</h2>
+				<p>Work hard. Pin hard.</p>
+				
+				<button type="button" class="btn btn-warning">Learn more</button>
+				<button type="button" class="btn btn-warning">Get started</button>
+			</div>
+			
+			<div class="col-md-5 topOffset">
+				
+				<h3>Register for free</h3>
+				
+				<form class="form" role="form">
+					
+					<div class="form-group">
+						<div class="col-sm-8 input-group">
+						
+							<span class="input-group-addon"><span class="fa fa-user"></span></span>
+							<input type="text" class="form-control" id="username" placeholder="Username">
+							
+						</div>
+					</div>
+					
+				  <div class="form-group">
+					<div class="col-sm-8 input-group">
+						<span class="input-group-addon"><span class="fa fa-asterisk"></span></span>
+						<input type="password" class="form-control" id="password" placeholder="Password">
+					</div>
+				  </div>
+				  
+				  <div class="form-group">
+					<div class="col-sm-8 input-group">
+						<span class="input-group-addon"><span class="fa fa-envelope"></span></span>
+						<input type="email" class="form-control" id="email" placeholder="Email">
+					</div>
+				  </div>
+				  
+				  <div class="form-group">
+					<div class="col-sm-8 input-group">
+					  <button type="submit" class="btn btn-danger btn-default">Sign Up</button>
+					</div>
+				  </div>
+				</form>
+			
+			</div>
+		
+		</div>
+		
+		<div class="row pvvoFooter">
+			<p class="">WomanInterest is a project for KaHo Sint&dash;Lieven University &dash; Developed by monkeys.</p>
+		</div>
+
+	</div>
+</div>
+
+<!--
+
+
 <p>Welcome!</p>
 <p>Check out the <a href="{{ URL::to('users') }}">Dummy users</a></p>
 <p>Check out the <a href="{{ URL::to('posts') }}">Dummy posts</a></p>
 <p>Check out the <a href="{{ URL::to('boards') }}">Dummy boards</a></p>
+
 
 @if(Auth::check())
 
 {{ Form::open(array('url' => 'posts/add')) }}
 
     <h1>Add post</h1>
-    <!-- if there are login errors, show them here -->
     <p> {{$errors->first('title')}}
         {{$errors->first('body')}}</p>
 
@@ -33,4 +151,6 @@ Welcome page
     <p>{{Form::submit('Submit')}}</p>
 {{ Form::close() }}
 @endif
+
+-->
 @stop
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,8 +2,6 @@
 <html lang="en" class="no-js">
 <head>
     <meta charset="utf-8">
-    {{ HTML::style('css/common.css'); }}
-    {{ HTML::script('js/testscript.js'); }}
     <title>@yield('pagetitle')</title>
 
     <meta name="description" content="">
@@ -13,33 +11,19 @@
 
     <link rel="shortcut icon" href="" />
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700|Dosis:200,600|Open+Sans:400,700" type="text/css" />
-	{{ HTML::style('css/bootstrap.min.css') }}
-	{{ HTML::style('css/bootstrap-theme.min.css') }}
+	{{ HTML::style('css/lumen/bootstrap.min.css') }}
+	{{ HTML::style('css/lumen/bootstrap-theme.min.css') }}
 	{{ HTML::style('css/font-awesome.min.css') }}
 	{{ HTML::style('css/default.css') }}
 	
 </head>
 
 <body>
-
-<div id="nav">
-    <ul>
-        <li><a href="{{ URL::TO('/') }}">home</a></li>
-        @if(Auth::check())
-        <li><a href="{{ URL::TO('admin/logout') }}">logout</a></li>
-        <li><a href="{{ URL::TO('admin/profile') }}">{{ Auth::user()->name  }} ({{ Auth::user()->username  }})</a></li>
-        @else
-        @if(!(Request::is('admin/login') || Request::is('admin/register')))
-            <li><a href="{{ URL::TO('admin/login') }}">Login</a> or <a href="{{ URL::TO('admin/register') }}">register</a></li>
-        @endif
-        @endif
-    </ul>
-</div>
-
 @yield('content')
 
 	<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	{{ HTML::script('js/bootstrap.min.js') }}
+
 </body>
 </html>
