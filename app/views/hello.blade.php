f7348c749802e743b2ff06178f4752ac29e88611
Jessestr <jesse.struyvelt@telenet.be>
2014-02-24 17:30:13 +0100

    First logged in view

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -64,41 +64,41 @@
 
 	<div class="container">
 	
-	<nav class="navbar navbar-default pvvoNavbar" role="navigation">
-	  <div class="container-fluid">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		  </button>
-		  <a class="navbar-brand pvvoTitle" href="#">WomanInterest</a>
-		</div>
+		<nav class="navbar navbar-default pvvoNavbar" role="navigation">
+		  <div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a class="navbar-brand pvvoTitle" href="#">WomanInterest</a>
+			</div>
 
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		  <ul class="nav navbar-nav">
-	
-		  </ul>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			  <ul class="nav navbar-nav">
 		
-		  <ul class="nav navbar-nav navbar-right">
-			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">USERS <span class="fa fa-bars leftSpacingSmall"></span></a>
-				<ul class="dropdown-menu pvvoDropdown text-right">
-					<li class="divider"></li>
-					<li><a href="" data-toggle="modal" data-target=".bs-example-modal-sm">Login <span class="fa fa-key leftSpacingSmall"> </span></a></li>
-					<li><a href="{{ URL::TO('admin/register') }}">Register <span class="fa fa-pencil leftSpacingSmall"> </span></a></li>
-					<li><a href="{{ URL::to('users') }}">Privacy <span class="fa fa-shield leftSpacingSmall"> </span></a></li>
-				</ul>
-			</li>
-			<li><a href="{{ URL::to('posts') }}">POSTS <span class="fa fa-comments leftSpacingSmall"></span></a></li>
-			<li><a href="{{ URL::to('boards') }}">BOARDS <span class="fa fa-cloud leftSpacingSmall"> </span> </a></li>
-		  </ul>
-		</div><!-- /.navbar-collapse -->
-	  </div><!-- /.container-fluid -->
-	</nav>
+			  </ul>
+			
+			  <ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">USERS <span class="fa fa-bars leftSpacingSmall"></span></a>
+					<ul class="dropdown-menu pvvoDropdown text-right">
+						<li class="divider"></li>
+						<li><a href="" data-toggle="modal" data-target=".bs-example-modal-sm">Login <span class="fa fa-key leftSpacingSmall"> </span></a></li>
+						<li><a href="{{ URL::TO('admin/register') }}">Register <span class="fa fa-pencil leftSpacingSmall"> </span></a></li>
+						<li><a href="{{ URL::to('users') }}">Privacy <span class="fa fa-shield leftSpacingSmall"> </span></a></li>
+					</ul>
+				</li>
+				<li><a href="{{ URL::to('posts') }}">POSTS <span class="fa fa-comments leftSpacingSmall"></span></a></li>
+				<li><a href="{{ URL::to('boards') }}">BOARDS <span class="fa fa-cloud leftSpacingSmall"> </span> </a></li>
+			  </ul>
+			</div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
 	
 		<div class="row">
 		
--- app/views/stream.blade.php
+++ app/views/stream.blade.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Glenn
- * Date: 2/24/14
- * Time: 5:16 PM
- */ 
+@extends('layouts.master')
+
+@section('pagetitle')
+	Welcome {{ Auth::user()->username  }}
+@stop
+
+@section('content')
+<div class="container">
+
+	<nav class="navbar navbar-default pvvoNavbar topOffset" role="navigation">
+	  <div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand pvvoTitle" href="#">WomanInterest</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav">
+
+		  </ul>
+		
+		  <ul class="nav navbar-nav navbar-right">
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">USERS <span class="fa fa-bars leftSpacingSmall"></span></a>
+				<ul class="dropdown-menu">
+					<li><a href="" data-toggle="modal" data-target=".bs-example-modal-sm">Login <span class="fa fa-key leftSpacingSmall"> </span></a></li>
+					<li><a href="{{ URL::TO('admin/register') }}">Register <span class="fa fa-pencil leftSpacingSmall"> </span></a></li>
+					<li class="divider"></li>
+					<li><a href="{{ URL::to('users') }}">Privacy <span class="fa fa-shield leftSpacingSmall"> </span></a></li>
+				</ul>
+			</li>
+			<li><a href="{{ URL::to('posts') }}">POSTS <span class="fa fa-comments leftSpacingSmall"></span></a></li>
+			<li><a href="{{ URL::to('boards') }}">BOARDS <span class="fa fa-cloud leftSpacingSmall"> </span> </a></li>
+		  </ul>
+		</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+	
+</div>
+@stop
