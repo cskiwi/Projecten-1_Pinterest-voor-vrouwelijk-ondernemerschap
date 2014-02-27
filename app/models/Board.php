c4f8e013fa81f809f89876675815e14a6b69718e
Glenn Latomme <glenn.latomme@gmail.com>
2014-02-27 12:24:46 +0100

    Test for -upstream on multiple git's

--- app/models/Board.php
+++ app/models/Board.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Class Board
+ */
 class Board extends Eloquent {
 
     /**
@@ -8,20 +11,41 @@ class Board extends Eloquent {
      * @var string
      */
     protected $table = 'boards';
+    /**
+     * @var array
+     */
     protected $fillable = array('user_id', 'title');
 
+    /**
+     * @var bool
+     */
     public $timestamps = true;
 
+    /**
+     * @return mixed
+     */
     public function Posts() {
         return $this->belongsToMany('Post', 'board_post');
     }
+
+    /**
+     * @return mixed
+     */
     public function User()
     {
         return $this->belongsTo('User');
     }
+
+    /**
+     * @return mixed
+     */
     public function Followers(){
         return $this->belongsToMany('User', 'follows', 'board_id'); //->withPivot('user_id');
     }
+
+    /**
+     * @return mixed
+     */
     public function Tags() {
         return $this->belongsToMany('Tag', 'board_tag');
     }
