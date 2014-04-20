dd8d110d6b41e5f77d7ea3918994d1b8e679e8d3
Jessestr <jesse.struyvelt@telenet.be>
2014-04-20 21:39:43 +0200

    User public profile design (begin)

--- app/views/users/profile.blade.php
+++ app/views/users/profile.blade.php
@@ -1,12 +1,116 @@
 @extends('layouts.master')
 
 @section('pagetitle')
-User detail | {{ $user->name }}
+	{{ $user->name }} on WomanInterest
 @stop
 
 @section('content')
 
-    <p>{{ $user->name }}</p>
+<div class="container">
+
+	<div class="panel">
+
+		<div class="panel-body">
+		
+			<div class="row">
+		
+				<div class="pull-left profBoxInfo">
+					<p><img src="http://placehold.it/150x150" /></p>
+				</div>
+				
+				<div class="pull-left profBoxInfo">
+					<h3>{{ $user->name }} - {{ $user->username }}</h3>
+					
+					<p>
+						<span class="fa fa-facebook leftSpacingSmall"></span>
+						<span class="fa fa-twitter leftSpacingSmall"></span>
+						<span class="fa fa-flickr leftSpacingSmall"></span>
+						<span class="fa fa-linkedin leftSpacingSmall"></span>
+					</p>
+				</div>
+			
+			</div>
+
+		</div>
+		
+
+	</div>
+	
+	<div class="row profBoxJumbo">
+			<ul class="stats">
+				<li class="profileTabActive">
+					<h4 class="statLbl">Pins</h4>
+					214
+				</li>
+				<li>
+					<h4 class="statLbl">Boards</h4>
+					15
+				</li>
+				<li>
+					<h4 class="statLbl">Followers</h4>
+					21k
+				</li>
+				<li>
+					<h4 class="statLbl">Favourites</h4>
+					852
+				</li>
+			</ul>
+		</div>
+	
+</div>
+<div class="well">
+<div class="container">
+	<div class="panel panel-default">
+		<div class="panel-heading"><h4>{{ $user->username }}'s boards</h4></div>
+		<div class="panel-body">
+
+			<div class="col-xs-6 col-md-12">
+				<a href="#" class="thumbnail boardThumb pull-left">
+					<p>Kittens</p>
+					<img src="http://placehold.it/150x150" >
+				</a>
+				<a href="#" class="thumbnail boardThumb pull-left">
+					<p>Kittens</p>
+					<img src="http://placehold.it/150x150" >
+				</a>
+				<a href="#" class="thumbnail boardThumb pull-left">
+					<p>Kittens</p>
+					<img src="http://placehold.it/150x150" >
+				</a>
+				<a href="#" class="thumbnail boardThumb pull-left">
+					<p>Kittens</p>
+					<img src="http://placehold.it/150x150" >
+				</a>
+				<a href="#" class="thumbnail boardThumb pull-left">
+					<p>Kittens</p>
+					<img src="http://placehold.it/150x150" >
+				</a>
+				<a href="#" class="thumbnail boardThumb pull-left">
+					<p>Kittens</p>
+					<img src="http://placehold.it/150x150" >
+				</a>
+				<a href="#" class="thumbnail boardThumb pull-left">
+					<p>Kittens</p>
+					<img src="http://placehold.it/150x150" >
+				</a>
+				<a href="#" class="thumbnail boardThumb pull-left">
+					<p>Kittens</p>
+					<img src="http://placehold.it/150x150" >
+				</a>
+				<a href="#" class="thumbnail boardThumb pull-left">
+					<p>Kittens</p>
+					<img src="http://placehold.it/150x150" >
+				</a>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+
+
+<div class="container">
+
 
 <h2>Has made the following boards</h2>
 <ul>
@@ -28,4 +132,7 @@ User detail | {{ $user->name }}
     <li><a href="{{ URL::TO('/boards/detail/'.$board->id) }}">{{ $board->title }}</a> ( has {{ count($board->posts) }} posts) </li>
     @endforeach
 </ul>
+
+</div>
+</div>
 @stop
