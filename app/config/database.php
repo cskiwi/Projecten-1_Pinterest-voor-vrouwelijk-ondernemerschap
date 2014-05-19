2f26e4a057bef4fe3ea979a7a6692a28a78c9b20
rzarector <vanhullenicolas@hotmail.com>
2014-05-19 12:17:38 +0200

    changes

--- app/config/database.php
+++ app/config/database.php
@@ -56,7 +56,7 @@ return array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
 			'database'  => 'pvvo',
-			'username'  => 'pvvo',
+			'username'  => 'root',
 			'password'  => 'Azerty123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
