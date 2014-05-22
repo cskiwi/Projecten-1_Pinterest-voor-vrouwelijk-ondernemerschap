572067c00e875fb33425a24aab3e351b9feb1475
Jessestr <jesse.struyvelt@telenet.be>
2014-05-22 14:50:25 +0200

    IK WIL NOG EENS PUSHEN

--- app/views/pins/search.blade.php
+++ app/views/pins/search.blade.php
@@ -4,13 +4,31 @@
 Board overview
 @stop
 
+@section('scripts')
+{{ HTML::script('//cdn.jsdelivr.net/isotope/1.5.25/jquery.isotope.min.js') }}
+{{ HTML::script('//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js') }}
+{{ HTML::script('js/jquery.touch-punch.min.js') }}
+{{ HTML::script('js/jquery.shapeshift.min.js') }}
+{{ HTML::script('js/followBoard.js') }}
+{{ HTML::script('js/stream.js') }}
+@stop
+
 @section('content')
-<div>
-    <ul>
-        @foreach($pins as $pin)
-        <li><a href="./detail/{{$pin->id}}">{{ $pin->title }}</a> by <a href="{{ URL::TO('/users/profile/'.$pin->user->id) }}"> {{$pin->user->name}}</a> </li>
-        @endforeach
-    </ul>
+
+
+<div class="well">
+    <h1>Search results</h1>
+    <div class="row topOffset">
+        <div class="col-md-12">
+            <div class="row photos" id="">
+
+                @foreach($pins as $pin)
+                @include('partial.pinboard')
+                @endforeach
+
+            </div>
+        </div>
+    </div>
 </div>
 
 @stop
